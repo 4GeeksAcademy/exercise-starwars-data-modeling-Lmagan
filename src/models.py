4b1ac96b5ce54d3878775ba649d0535f1884c0b6
L.Magán <lucasfliper@hotmail.com>
2025-02-03 16:09:47 +0000

    first_commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,99 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False)
+    full_name = Column(String(50), nullable=False)
+    email = Column(String(80), nullable=False, unique=True)
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    favorite = relationship('Favorite', backref='user',lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'planet'
+    
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(100), nullable=False)
+    diameter = Column(String(100), nullable=False)
+    rotation_period = Column(String(100), nullable=False)
+    orbital_period = Column(String(100), nullable=False)
+    gravity = Column(String(100), nullable=False)
+    population = Column(String(100), nullable=False)
+    climate = Column(String(100), nullable=False)
+    terrain = Column(String(100), nullable=False)
+    surface_water = Column(String(100), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    birth_year = Column(String(100), nullable=False)
+    eye_color = Column(String(100), nullable=False)
+    gender = Column(String(100), nullable=False)
+    hair_color = Column(String(100), nullable=False)
+    height = Column(String(20), nullable=False)
+    mass = Column(String(40), nullable=False)
+    skin_color = Column(String(20), nullable=False)
+    homeworld = Column(String(40), nullable=False)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    model = Column(String(100), nullable=False)
+    vehicle_class = Column(String(100), nullable=False)
+    manufacturer = Column(String(100), nullable=False)
+    length = Column(String(100), nullable=False)
+    cost_in_credits = Column(String(100), nullable=False)
+    crew = Column(String(100), nullable=False)
+    max_atmosphering_speed = Column(String(100), nullable=False)
+    cargo_capacity = Column(String(100), nullable=False)
+    consumables = Column(String(100), nullable=False)
+
+class Film(Base):
+    __tablename__ = 'film'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    year = Column(Integer, nullable=False)
+    director = Column(String(80), nullable=False)
+    character = Column(Integer, ForeignKey('character.id'))
+    vehicle = Column(Integer, ForeignKey('vehicle.id'))
+    planet = Column(Integer, ForeignKey('planet.id'))
+
+class Favorite_vehicle(Base):
+    __tablename__ = 'favorite_vehicle'
+
+    id = Column(Integer, primary_key=True)
+    vehicle = Column(Integer, ForeignKey('vehicle.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Favorite_planet(Base):
+    __tablename__ = 'favorite_planet'
+
+    id = Column(Integer, primary_key=True)
+    planet = Column(Integer, ForeignKey('planet.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Favorite_film(Base):
+    __tablename__ = 'favorite_film'
+
+    id = Column(Integer, primary_key=True)
+    film = Column(Integer, ForeignKey('film.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+class Favorite_character(Base):
+    __tablename__ = 'favorite_character'
+
+    id = Column(Integer, primary_key=True)
+    character = Column(Integer, ForeignKey('character.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+   
 
     def to_dict(self):
         return {}
